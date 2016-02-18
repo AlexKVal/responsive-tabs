0ce8c30cb2b34f8016a9fd4a917356326a9fed81
AlexKVal <alexkval@gmail.com>
2016-02-18 20:30:02 +0200

    rewrite with event delegation

--- main.js
+++ main.js
@@ -3,49 +3,54 @@
 
   var tabs = function tabs(options) {
     var el = document.querySelector(options.el);
-    var tabNavigationLinks = el.querySelectorAll(options.tabNavigationLinks);
-    var tabContentContainers = el.querySelectorAll(options.tabContentContainers);
+    var activeTab = options.activeTab;
 
-    var activeIndex = 0;
     var initCalled = false;
-
     var init = function() {
-      if (initCalled) { return; }
-
+      if (initCalled) return;
       el.classList.remove('no-js');
-
-      // TODO: rewrite this with event delegation to a parent element
-      for (var i = 0; i < tabNavigationLinks.length; i++) {
-        addOnClickHandler(tabNavigationLinks[i], i);
-      }
-
+      hookupOnClickListener(el);
       initCalled = true;
     };
 
-    var addOnClickHandler = function(link, index) {
-      link.addEventListener('click', function(e){
-          e.preventDefault();
-          showTab(index);
-        });
+    var hookupOnClickListener = function(el) {
+      el.onclick = function(event) {
+        // IE doesn't pass in the event object
+        event = event || window.event;
+        // IE uses srcElement as the target
+        var eTarget = event.target || event.srcElement;
+
+        // traversal
+        while (eTarget !== el) {
+          if (eTarget.tagName === 'A') {
+            var href = eTarget.getAttribute('href');
+            var tabTarget = (href.charAt(0) === '#') && href;
+
+            if (tabTarget) {
+              showTab(tabTarget);
+              return;
+            }
+          }
+          eTarget = eTarget.parentNode;
+        }
+      };
     };
 
     function activeOn(el) { el.classList.add('is-active') };
     function activeOff(el) { el.classList.remove('is-active') };
 
-    var showTab = function(newIndex) {
-      if (
-        newIndex === activeIndex ||
-        newIndex < 0 ||
-        newIndex > tabNavigationLinks.length
-      ) { return; }
+    var showTab = function(tabId) {
+      var tabToShow = document.querySelector(tabId);
 
-      activeOff(tabNavigationLinks[activeIndex]);
-      activeOn(tabNavigationLinks[newIndex]);
+      if (! tabToShow || tabToShow === activeTab) return;
 
-      activeOff(tabContentContainers[activeIndex]);
-      activeOn(tabContentContainers[newIndex]);
+      activeOff(document.querySelector('a.is-active'));
+      activeOn(document.querySelector('a[href="'+tabId+'"]'));
 
-      activeIndex = newIndex;
+      activeOff(activeTab);
+      activeOn(tabToShow);
+
+      activeTab = tabToShow;
     };
 
     return {
